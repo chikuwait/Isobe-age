ee1a19b68b029e06e12005ed1e43fe508b2f0470
chikuwait <chikuwa.1703@gmail.com>
2018-06-02 11:29:42 +0900

    add package for criu build

--- cookbooks/apt-update.rb
+++ cookbooks/apt-update.rb
@@ -1,4 +1,6 @@
 execute "apt -y update" do
   user "root"
 end
-
+execute "apt -y install libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf" do
+  user "root"
+end
