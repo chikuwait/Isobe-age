103a425fc24074d5cdd8633b1a2c2f6c3e1af8de
chikuwait <chikuwa.1703@gmail.com>
2018-06-13 13:27:38 +0000

    add criu cookbooks

--- cookbooks/apt.rb
+++ cookbooks/apt.rb
@@ -1,6 +1,6 @@
 execute "apt -y update" do
   user "root"
 end
-execute "apt -y install libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf" do
+execute "apt -y install asciidoc libprotobuf-dev libprotobuf-c0-dev protobuf-c-compiler protobuf-compiler python-protobuf libnet1-dev libnl-3-dev libcap-dev" do  
   user "root"
 end
--- cookbooks/base.rb
+++ cookbooks/base.rb
@@ -1,3 +1,4 @@
 include_recipe "apt.rb"
 include_recipe "ruby.rb"
+include_recipe "criu.rb"
 include_recipe "mruby.rb"
